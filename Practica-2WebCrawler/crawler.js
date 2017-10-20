91599d99d1c14a4e0d74ad9727caef4b8414d99c
themostretarded <marcosresidentevil@hotmail.com>
2017-10-20 18:50:28 -0500

    Cambio de direccion
    la otra direccion esta refea

--- Practica-2WebCrawler/crawler.js
+++ Practica-2WebCrawler/crawler.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-var pageToVisit = "http://www.imdb.com/trailers?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3320158662&pf_rd_r=0H4SW16FRXKTXJQKCJTF&pf_rd_s=hero&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_hp_sm";
+var pageToVisit = "http://www.imdb.com/?ref_=nv_home";
 console.log("Visiting page " + pageToVisit);
 request(pageToVisit, function(error, response, body) {
    if(error) {
