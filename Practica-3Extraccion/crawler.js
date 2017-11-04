c4223f59cb732759a7e7de2fe7ad324cf606a3a9
Marcos Ernesto <marcosresidentevil@hotmail.com>
2017-11-04 13:33:24 -0600

    Delete crawler.js

--- Practica-3Extraccion/crawler.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-var cheerio = require('cheerio');
-
-request('http://www.imdb.com/movies-coming-soon/?ref_=inth_cs', function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      console.log(html);
-    }
-  });
