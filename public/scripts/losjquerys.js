d15cd764869b586fa958f7bd6ae6c1ffae8ec7d9
themostretarded <marcosresidentevil@hotmail.com>
2017-12-06 17:30:33 -0600

    intentos

--- public/scripts/losjquerys.js
+++ public/scripts/losjquerys.js
@@ -10,22 +10,5 @@
 const url = "https://api.myjson.com/bins/i1gx3"
 
 $(() => {
-    //consumir api 
-    /*
-    $.getJSON(url,data=>{
-      var item = [];
-      $.each();
-    }); */
-    $.getJSON( url, ( data ) => {
-      var items = [];
-      $.each( data, function( key, val ) {
-        items.push( "<li id='" + key + "'>" + val + "</li>" );
-        console.log("insercion arreglo")
-      });
-     
-      /*$( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-      }).appendTo( "body" );*/
-    });
+   
 });
