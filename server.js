eef20c45f9970d108043bcb6191dbcd57fd29666
themostretarded <marcosresidentevil@hotmail.com>
2017-12-13 17:12:50 -0600

    Listo Frontend

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Nada');
-});
-
-app.get('/comedia', function (req, res) {
-  res.send('api lista');
-});
-
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-/*var http = require('http');
+/*
+app.get('/', function (req, res) {
+  res.send('Nada');
+});
+app.use('/static', express.static('public'));
+var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
