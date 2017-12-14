bc54e0878acc674f8d875636c2bec6c4cb1021bf
themostretarded <marcosresidentevil@hotmail.com>
2017-12-13 18:34:02 -0600

    cambio del puerto

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
 });
 
 /*
